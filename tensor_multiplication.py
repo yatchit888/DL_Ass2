304053f4c43224f5924569e45fb75c7504761202
yatchit888 <yatchit888@gmail.com>
2024-01-18 22:15:43 -0500

    Complete the all function

--- tensor_multiplication.py
+++ tensor_multiplication.py
@@ -3,13 +3,13 @@ import torch
 def create_tensor_of_val(dimensions, val):
     """
     Create a tensor of the given dimensions, filled with the value of `val`.
-    dimentions is a tuple of integers.
+    dimensions is a tuple of integers.
     Hint: use torch.ones and multiply by val, or use torch.zeros and add val.
     e.g. if dimensions = (2, 3), and val = 3, then the returned tensor should be of shape (2, 3)
     specifically, it should be:
     tensor([[3., 3., 3.], [3., 3., 3.]])
     """
-    res = None  # TODO: implement this function 
+    res = torch.ones(dimensions) * val
     return res
 
 def calculate_elementwise_product(A, B):
@@ -17,8 +17,8 @@ def calculate_elementwise_product(A, B):
     Calculate the elementwise product of the two tensors A and B.
     Note that the dimensions of A and B should be the same.
     """
-    res = None  # TODO: implement this function 
-    return res 
+    res = torch.mul(A, B)
+    return res
 
 
 def calculate_matrix_product(X, W):
@@ -26,13 +26,12 @@ def calculate_matrix_product(X, W):
     Calculate the product of the two tensors X and W. ( sum {x_i * w_i})
     Note that the dimensions of X and W should be compatible for multiplication.
     e.g: if X is a tensor of shape (1,3) then W could be a tensor of shape (N,3) i.e: (1,3) or (2,3) etc. but in order for 
-         matmul to work, we need to multiply by W.T (W transpose) so that the `inner` dimensions are the same.
+       matmul to work, we need to multiply by W.T (W transpose) so that the `inner` dimensions are the same.
     Hint: use torch.matmul to calculate the product.
-          This allows us to use a batch of inputs, and not just a single input.
-          Also, it allows us to use the same function for a single neuron or multiple neurons.
-         
+        This allows us to use a batch of inputs, and not just a single input.
+        Also, it allows us to use the same function for a single neuron or multiple neurons.
     """
-    res = None  # TODO: implement this function 
+    res = torch.matmul(X, W.T)
     return res
 
 def calculate_matrix_prod_with_bias(X, W, b):
@@ -40,12 +39,12 @@ def calculate_matrix_prod_with_bias(X, W, b):
     Calculate the product of the two tensors X and W. ( sum {x_i * w_i}) and add the bias.
     Note that the dimensions of X and W should be compatible for multiplication.
     e.g: if X is a tensor of shape (1,3) then W could be a tensor of shape (N,3) i.e: (1,3) or (2,3) etc. but in order for
-         matmul to work, we need to multiply by W.T (W transpose) so that the `inner` dimensions are the same.
+       matmul to work, we need to multiply by W.T (W transpose) so that the `inner` dimensions are the same.
     Hint: use torch.matmul to calculate the product.
-          This allows us to use a batch of inputs, and not just a single input.
-          Also, it allows us to use the same function for a single neuron or multiple neurons.
-       """
-    res = None  # TODO: implement this function 
+        This allows us to use a batch of inputs, and not just a single input.
+        Also, it allows us to use the same function for a single neuron or multiple neurons.
+    """
+    res = torch.matmul(X, W.T) + b
     return res
 
 def calculate_activation(sum_total):
@@ -53,7 +52,7 @@ def calculate_activation(sum_total):
     Calculate a step function as an activation of the neuron.
     Hint: use PyTorch `heaviside` function.
     """
-    res = None  # TODO: implement this function 
+    res = torch.heaviside(sum_total, torch.tensor([0.]))
     return res
 
 def calculate_output(X, W, b):
@@ -61,5 +60,6 @@ def calculate_output(X, W, b):
     Calculate the output of the neuron.
     Hint: use the functions you implemented above.
     """
-    res = None  # TODO: implement this function 
-    return res
+    sum_total = calculate_matrix_prod_with_bias(X, W, b)
+    output = calculate_activation(sum_total)
+    return output
