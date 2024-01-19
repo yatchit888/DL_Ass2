dcd07691889c23e2dc1bd592a49c7210b703317e
Yat Chit Law <85443779+yatchit888@users.noreply.github.com>
2024-01-18 21:39:51 -0500

    Add files via upload

---
+++ numeric_derivation.py
@@ -0,0 +1,3 @@
+
+def derive(f, x, h=0.0001):
+    return None  # TODO: implement this function 
---
+++ rmse.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def rmse(predictions, targets):
+    pred = np.array(predictions)
+    tar = np.array(targets)
+    rmse = None # TODO: Implement RMSE Calculation here...
+    return rmse
